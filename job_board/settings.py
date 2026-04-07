1e6077f3ba50c8e09d8a21a91d031144d2c48699
tima <fatima.ezzahra.belkoudia@gmail.com>
2026-04-07 14:35:47 +0200

    Add Docker Compose configuration and update settings for PostgreSQL integration

--- job_board/settings.py
+++ job_board/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v++r+fn6w$j+yr3ye5+_qnqepn*h(n@i$-@hem%b=t3@4za%wp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('APP_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', os.getenv('APP_DOMAIN', 'localhost')]
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://' + os.getenv('APP_DOMAIN', 'localhost')]
 
 
 # Application definition
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'job_board.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'mydatabase'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
